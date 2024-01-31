b8d2386131af76eabb53109e2bfe57bd514fed06
Satvik <satvik.srivastavaaa@gmail.com>
2024-01-31 15:12:15 +0530

    Add files via upload

---
+++ Java/function.java
@@ -0,0 +1,71 @@
+// import java.util.Scanner;
+
+public class function {
+
+    // public static void sing(){
+
+    //     System.out.println("hello");
+    //     System.out.println("hello");
+    // }
+
+
+    //  public static double area(double length , double width){
+
+    //     double area = length * width;
+    //     // System.out.println(area);
+
+    //     // System.out.println("Area - " + length*width);
+    //     return area; 
+
+    // }
+
+
+    /**
+     * 
+     * @param satvik 
+     * @param name
+     * @param age
+     */
+
+    // public static void greet(String name , Integer age){
+
+    //     System.out.println("name - " + name + "  Age - " + age);
+
+    
+    
+
+    public static void main(String[] args) {
+        
+        // sing();
+
+        // Scanner scanner = new Scanner(System.in);
+
+        // double length = scanner.nextDouble();
+        // double width = scanner.nextDouble();
+        // // area(length, width);
+        // // System.out.println(area(length, width));
+
+
+        // Scanner scanner = new Scanner(System.in);
+
+        // String name = scanner.nextLine();
+        // int age = scanner.nextInt();
+
+        // greet(name, age);
+
+        String string = "satvik hello";
+        System.out.println(string);
+        System.out.println(string.length());
+        System.out.println(string.substring(3));
+        System.out.println(string.toUpperCase());
+
+        String string2 = " Satvik ";
+        String updatedString = string2.replace('a', 'o');
+    
+        System.out.println(updatedString);
+
+        
+
+    }
+                                                     
+}
